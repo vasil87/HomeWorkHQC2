247d98fa276932f8810b7d71dd2fb4ad48693aec
vasil kamburov <vasil.kamburov@abv.bg>
2016-10-04 18:01:24 +0300

    Error Logging with log4net

--- RotatingWalkInMatrix/StartUp.cs
+++ RotatingWalkInMatrix/StartUp.cs
@@ -4,18 +4,33 @@
     using Common;
     using Contracts;
     using Models;
+    using log4net;
+    using log4net.Config;
 
     public class RotatingWalkInMatrix
     {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(RotatingWalkInMatrix));
         private const int NumberOfMatrixRowsAndCows = 6;
         private const int StartingPositionOnRowsAndCows = 0;
 
         public static void Main()
-        {       
-            int rowsAndCowsInTheMatrix = NumberOfMatrixRowsAndCows;
-            ICell startingCell = new Cell(StartingPositionOnRowsAndCows, StartingPositionOnRowsAndCows);
-            IMatrix matrix = new Matrix(rowsAndCowsInTheMatrix);
-            IMatrixUtils matrixUtils = new MatrixUtils(matrix, PossibleDirections.AllDirections, startingCell);
+        {
+            XmlConfigurator.Configure();
+            IMatrix matrix=default(IMatrix);
+            IMatrixUtils matrixUtils=default(IMatrixUtils);
+
+            try
+            {
+                int rowsAndCowsInTheMatrix = NumberOfMatrixRowsAndCows;
+                ICell startingCell = new Cell(StartingPositionOnRowsAndCows, StartingPositionOnRowsAndCows);
+                matrix = new Matrix(rowsAndCowsInTheMatrix);
+                matrixUtils = new MatrixUtils(matrix, PossibleDirections.AllDirections, startingCell);
+                throw new ArgumentException("I Have Error Loging");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("Initializing of Matrix failed", ex);
+            }
 
             matrixUtils.FillMatrix();
 

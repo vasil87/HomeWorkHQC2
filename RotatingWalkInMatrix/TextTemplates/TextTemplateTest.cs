d79d7920c2b3c8f9183b305a13f117e49b30f5fd
vasil kamburov <vasil.kamburov@abv.bg>
2016-10-04 22:59:33 +0300

    All

---
+++ RotatingWalkInMatrix/TextTemplates/TextTemplateTest.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Test
+{
+   enum Month
+   {
+                  November,
+		             December,
+		             January,
+		             February,
+		             March,
+		             April,
+		             May,
+		             June,
+		             July,
+		             August,
+		             September,
+		             October,
+		   }
+}
